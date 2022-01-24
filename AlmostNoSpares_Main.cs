ba4d93c1f3dac2bdde3940bb3a6ff81c392e9be4
Zemogiter <zumi999@vp.pl>
2022-01-24 21:34:33 +0100

    Add files via upload

---
+++ AlmostNoSpares_Main.cs
@@ -0,0 +1,36 @@
+﻿using Planetbase;
+using System;
+using UnityEngine;
+using System.Reflection;
+using System.IO;
+
+namespace Tahvohck_Mods
+{
+    public class AlmostNoSpares
+    {
+        [LoaderOptimization(LoaderOptimization.NotSpecified)]
+        public static void Init()
+        {
+            try
+            {
+                TahvohckUtil.FirstUpdate += Update;
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e.Message);
+            }
+        }
+
+        public static void Update()
+        {
+            ModuleName solar = ModuleName.SolarPanel;
+            typeof(ModuleTypeSolarPanel)
+                .GetField("mCondicionDecayTime", BindingFlags.NonPublic | BindingFlags.Instance)
+                .SetValue(solar, 14000f);
+            ModuleName wind = ModuleName.WindTurbine;
+            typeof(ModuleTypeWindTurbine)
+                .GetField("mCondicionDecayTime", BindingFlags.NonPublic | BindingFlags.Instance)
+                .SetValue(wind, 14000f);
+        }
+    }
+}
